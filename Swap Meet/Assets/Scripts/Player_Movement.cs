922258c74bb7f7d6210b7074324375ca18bc1251
Matthew Jackson <102381833@student.swin.edu.au>
2020-07-11 14:48:30 +1000

    Created the Default Player objects and basic movement controls

--- Swap Meet/Assets/Scripts/Player_Movement.cs
+++ Swap Meet/Assets/Scripts/Player_Movement.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int speed = 5;
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 move = new Vector3();
+        move += new Vector3((Input.GetAxis("Horizontal")* speed) * Time.deltaTime, 0f, 0f);
+        move += new Vector3(0f, 0f, (Input.GetAxis("Vertical")* speed) * Time.deltaTime);
+        transform.Translate(move); 
         
     }
 }
